df507b8d2891088b0ad389a536c143bb48422269
nelson truran <nelson@truran.dev>
2025-01-07 22:07:35 -0800

    fix screen size

--- src/Game/Game.cpp
+++ src/Game/Game.cpp
@@ -7,60 +7,76 @@
 
 #include "../States/MenuState.hpp"
 
-Game::Game() : m_window(sf::VideoMode({1920, 1080}), "Game of Intrigue") {
+Game::Game() : m_window(sf::VideoMode({1920, 1080}), "Game of Intrigue", sf::Style::Close)
+{
     m_window.setFramerateLimit(144);
     pushState(std::make_unique<MenuState>(m_window));
 }
 
-void Game::run() {
-    while (m_window.isOpen()) {
+void Game::run()
+{
+    while (m_window.isOpen())
+    {
         handleEvents();
         update();
         render();
     }
 }
 
-void Game::handleEvents() {
-    while (const std::optional<sf::Event> event = m_window.pollEvent()) {
-        if (event->is<sf::Event::Closed>()) {
-            m_window.close();
-        }
-        if (!event.has_value()) {
+void Game::handleEvents()
+{
+    while (const std::optional event = m_window.pollEvent())
+    {
+        if (!event.has_value())
+        {
             continue;
         }
-        if (!m_states.empty()) {
+        if (event->is<sf::Event::Closed>())
+        {
+            m_window.close();
+            break;
+        }
+        if (!m_states.empty())
+        {
             m_states.top()->handleEvent(event.value());
         }
     }
 }
 
-
-void Game::update() {
-    if (!m_states.empty()) {
+void Game::update()
+{
+    if (!m_states.empty())
+    {
         m_states.top()->update();
     }
 }
 
-void Game::render() {
+void Game::render()
+{
     m_window.clear();
-    if (!m_states.empty()) {
+    if (!m_states.empty())
+    {
         m_states.top()->render(m_window);
     }
     m_window.display();
 }
 
-void Game::pushState(std::unique_ptr<GameState> state) {
+void Game::pushState(std::unique_ptr<GameState> state)
+{
     state->onEnter();
     m_states.push(std::move(state));
 }
 
-void Game::popState() {
-    if (!m_states.empty()) {
+void Game::popState()
+{
+    if (!m_states.empty())
+    {
         m_states.top()->onExit();
         m_states.pop();
     }
 }
 
-std::unique_ptr<GameState> &Game::getCurrentState() {
+std::unique_ptr<GameState> &Game::getCurrentState()
+{
     return m_states.top();
 }
