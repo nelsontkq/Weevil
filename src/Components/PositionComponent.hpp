1b822e265f212235faf57e3c08bbc4ab329d9604
nelson truran <nelson@truran.dev>
2025-01-10 18:30:52 -0800

    tmp

--- src/Components/PositionComponent.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef POSITION_COMPONENT_HPP
-#define POSITION_COMPONENT_HPP
-
-struct PositionComponent {
-    float x;
-    float y;
-};
-
-#endif // POSITION_COMPONENT_HPP
--- src/States/MainGameState.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by nelson on 1/5/25.
-//
-
-#include "MainGameState.hpp"
