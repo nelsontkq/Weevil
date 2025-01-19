4a3e8a53433e38299e551ce231abc027f664b25d
nelson truran <nelson@truran.dev>
2025-01-18 22:53:44 -0800

    SDL3

---
+++ include/WeevilEngine/AssetManager.h
@@ -0,0 +1,14 @@
+//
+// Created by nelson on 1/18/25.
+//
+
+#ifndef WEEVIL_INCLUDE_WEEVILENGINE_ASSETMANAGER_H_
+#define WEEVIL_INCLUDE_WEEVILENGINE_ASSETMANAGER_H_
+#include "wvpch.h"
+
+class AssetManager {
+  auto load(std::filesystem::path) -> u_int64_t;
+
+};
+
+#endif //WEEVIL_INCLUDE_WEEVILENGINE_ASSETMANAGER_H_
